0712ac76d688d9cb0cadedbf587031b8ac6dc8b2
Steve Zesch <stevezesch2@gmail.com>
2013-11-27 14:54:27 -0500

    Fixed typo in help_cb
    closes #21

--- src/math-window.c
+++ src/math-window.c
@@ -222,7 +222,7 @@ static void help_cb(GtkWidget *widget, MathWindow *window)
         d = gtk_message_dialog_new(GTK_WINDOW(window), GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
                 GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s", message);
         gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(d), "%s", error->message);
-        g_signal_connect(d, "respones", G_CALLBACK(gtk_widget_destroy), NULL);
+        g_signal_connect(d, "response", G_CALLBACK(gtk_widget_destroy), NULL);
         gtk_window_present(GTK_WINDOW (d));
 
         g_error_free(error);
