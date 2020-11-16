ea1c2aaa6ad1eae1bb199c7d2e5ff1e144da7f61
rbuj <robert.buj@gmail.com>
2020-11-16 08:35:13 +0100

    Remove USE_MATE2_MACROS from autogen.sh (legacy)

--- autogen.sh
+++ autogen.sh
@@ -21,8 +21,8 @@ which mate-autogen || {
 }
 
 which yelp-build || {
-    echo "You need to install yelp-tools" 
+    echo "You need to install yelp-tools"
     exit 1
 }
 
-USE_MATE2_MACROS=1 USE_COMMON_DOC_BUILD=yes . mate-autogen
+USE_COMMON_DOC_BUILD=yes . mate-autogen
