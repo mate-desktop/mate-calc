a942e94c8a240452b64e2bf1fa56e3fc4a5031f1
raveit65 <mate@raveit.de>
2021-06-21 17:08:29 +0200

    update copyright to 2021

--- src/math-history-entry.c
+++ src/math-history-entry.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 MATE developers
+ * Copyright (C) 2020-2021 MATE developers
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
