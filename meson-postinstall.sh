8dca108a6a4bed0b3411e41cb04b143c71036b67
zhuyaliang <15132211195@163.com>
2021-07-25 11:52:47 +0800

    Add build support for meson

---
+++ meson-postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Package managers set this so we don't need to run
+
+if [ -z "$DESTDIR" ]; then
+  echo Compiling GSettings schemas...
+  glib-compile-schemas ${MESON_INSTALL_PREFIX}/share/glib-2.0/schemas
+
+  echo Updating desktop database...
+  update-desktop-database -q ${MESON_INSTALL_PREFIX}/share/applications
+
+  ln -sf ${MESON_INSTALL_PREFIX}/bin/'mate-calc' ${MESON_INSTALL_PREFIX}/bin/'mate-calculator'
+fi
